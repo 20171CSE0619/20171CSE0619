193e632d0a4290b36b03d9849cc4b3ced0d97912
20171CSE0619 <60249623+20171CSE0619@users.noreply.github.com>
2020-01-24 13:16:50 +0530

    Add files via upload

---
+++ e7.c
@@ -0,0 +1,11 @@
+/*Program to print the size of datatype*/
+#include<stdio.h>
+void main()
+{
+printf("Size of character type is %ld\n",sizeof(char));
+printf("Size of integer type is %ld\n",sizeof(int));
+printf("Size of float is %ld\n",sizeof(float));
+printf("Size of double is %ld\n",sizeof(double));
+printf("Size of short int is %ld\n",sizeof(short));
+printf("Size of long int is%ld\n",sizeof(long));
+}
